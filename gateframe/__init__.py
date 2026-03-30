faf8020b784b30068003490b7f484e74fbfbba79
practicalmind-dev <baranyallcin@gmail.com>
2026-03-30 10:41:19 +0300

    refactor: expose top-level public API via __init__.py

--- gateframe/__init__.py
+++ gateframe/__init__.py
@@ -1,1 +1,24 @@
+from gateframe.core.context import WorkflowContext
+from gateframe.core.contract import ValidationContract, ValidationResult
+from gateframe.core.escalation import EscalationRoute, EscalationRouter
+from gateframe.core.failure import FailureMode, FailureResult
+from gateframe.rules.boundary import AllowedValues, BoundaryRule
+from gateframe.rules.confidence import ConfidenceRule
+from gateframe.rules.semantic import LlmJudge, SemanticRule
+from gateframe.rules.structural import StructuralRule
 
+__all__ = [
+    "AllowedValues",
+    "BoundaryRule",
+    "ConfidenceRule",
+    "EscalationRoute",
+    "EscalationRouter",
+    "FailureMode",
+    "FailureResult",
+    "LlmJudge",
+    "SemanticRule",
+    "StructuralRule",
+    "ValidationContract",
+    "ValidationResult",
+    "WorkflowContext",
+]
